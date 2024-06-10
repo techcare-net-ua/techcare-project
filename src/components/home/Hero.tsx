a4e88d540f09472760788a647b1554b2dc994918
GermanSTr <gershev1994@gmail.com>
2024-06-10 16:49:08 +0300

    fix: style for lg screen

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -28,32 +28,38 @@ export const Hero: React.FC = () => {
         <div className="absolute bottom-0 left-0 hidden h-20 w-screen bg-gradient-to-t from-background to-transparent dark:block"></div>
       </div>
       <Container
-        className="flex flex-col items-center pb-5 pt-20 md:flex-row md:pt-28 lg:justify-between lg:px-16 lg:pt-32
+        className="flex flex-col items-center pb-5 pt-20 md:flex-row md:pt-28 xl:justify-between xl:px-16 xl:pt-32
       "
       >
-        <div className="space-y-10 pb-10 md:space-y-16 md:pr-4 lg:w-[800px]">
-          <h1 className="text-primary-foreground opacity-80 drop-shadow-2xl md:text-left md:text-5xl lg:text-center lg:text-7xl lg:font-bold dark:text-foreground">
+        <div className="space-y-10 pb-10 md:space-y-16 md:pr-4 xl:w-[800px]">
+          <h1
+            className="text-center text-primary-foreground opacity-90 drop-shadow-3xl md:text-left 
+          md:text-5xl xl:text-left xl:text-7xl xl:font-bold dark:text-foreground"
+          >
             Techcare - надійний провідник у світ інформаційних технологій
           </h1>
-          <p className="mx-auto w-[400px] text-primary-foreground md:mx-0 lg:w-full  lg:text-xl">
+          <p
+            className="mx-auto w-[400px] text-primary-foreground md:mx-0 lg:w-full xl:w-full xl:text-xl 
+          dark:text-foreground"
+          >
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
             consequatur ea veritatis consectetur expedita reiciendis fugiat
             culpa tempora dolores voluptatem.
           </p>
         </div>
-        <Card className="mt-0 w-[400px] rounded-lg opacity-80 md:max-w-[400px]  lg:w-[500px]">
-          <CardHeader className="lg:space-y-3">
-            <CardTitle className="lg:text-3xl">
+        <Card className="mt-0 w-[400px] rounded-lg opacity-80 md:max-w-[400px]  xl:w-[500px]">
+          <CardHeader className="xl:space-y-3">
+            <CardTitle className="xl:text-3xl">
               Потрібна допомога системного адміністратора?
             </CardTitle>
-            <CardDescription className="lg:text-lg">
+            <CardDescription className="xl:text-lg">
               Залиште номер - ми зателефонуємо.
             </CardDescription>
           </CardHeader>
           <CardContent>
             <form>
-              <div className="grid w-full items-center gap-4 lg:gap-5">
-                <div className="flex flex-col space-y-3.5 lg:space-y-4">
+              <div className="grid w-full items-center gap-4 xl:gap-5">
+                <div className="flex flex-col space-y-3.5 xl:space-y-4">
                   <Input id="name" placeholder="Телефон" />
                   <Input id="name" placeholder="Ім'я або питання" />
                 </div>
