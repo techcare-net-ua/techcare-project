37afaefc58980f0d061a9203b38c554f5a29d39c
ilya9293 <ilya9293@ukr.net>
2024-09-05 11:04:28 +0300

    feat:add fields in contacts feedback

--- src/components/contacts/FeedbackSection/FeedbackSection.tsx
+++ src/components/contacts/FeedbackSection/FeedbackSection.tsx
@@ -16,12 +16,22 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const formSchema = z.object({
-   fullname: z.string().min(2, {
+  fullname: z.string().min(2, {
     message: 'Username must be at least 2 characters.',
   }),
   compname: z.string().min(2, {
+    message: 'Company name must be at least 2 characters.',
+  }),
+  email: z.string().min(2, {
+    message: 'Company name must be at least 2 characters.',
+  }),
+  phone: z.string().min(2, {
+    message: 'Company name must be at least 2 characters.',
+  }),
+  about: z.string().min(2, {
     message: 'Company name must be at least 2 characters.',
   }),
 });
@@ -33,6 +43,9 @@ export const FeedbackSection = () => {
     defaultValues: {
       fullname: '',
       compname: '',
+      email: '',
+      phone: '',
+      about: '',
     },
   });
 
@@ -46,40 +59,92 @@ export const FeedbackSection = () => {
   return (
     <section>
       <Container>
-        <Card className="mt-0 max-w-[400px] rounded-lg opacity-80 md:max-w-[400px]  xl:w-[500px]">
+        <Card className="mx-auto rounded-lg opacity-80 max-w-6xl">
           <CardContent className="p-6">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
+                className="grid gap-8"
               >
+                <div className='grid gap-8 md:grid-cols-2'>
+                  <FormField
+                    control={form.control}
+                    name="fullname"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Full name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Full name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="compname"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Company name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Company name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Email" type="email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Phone number</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Phone number"
+                            type="phone"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
                 <FormField
                   control={form.control}
-                  name="fullname"
+                  name="about"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Full name</FormLabel>
+                      <FormLabel>About project</FormLabel>
                       <FormControl>
-                        <Input placeholder="Full name" {...field} />
+                        <Textarea
+                          placeholder="Tell us about your project"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
 
-                <FormField
-                  control={form.control}
-                  name="compname"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Company name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Company name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
                 <Button type="submit" className="w-full">
                   Надіслати
                 </Button>
